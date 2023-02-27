bb07e092e7d198235a9e841181ae3539eb90fb0e
sahadevan <sahadevan511@gmail.com>
2023-02-27 11:57:35 +0530

    Use environment variables instead of hard coded values

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,6 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')  
+DB_USER = os.getenv('DB_USER', 'postgres')  
+DB_PASSWORD = os.getenv('DB_PASSWORD', '')  
+DB_NAME = os.getenv('DB_NAME', 'trivia_test')  
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
@@ -13,8 +19,8 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_name = DB_NAME
+        self.database_path = DB_PATH
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
